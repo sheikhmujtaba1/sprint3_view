6579c4062afa1fa5c3f8447e6312a962b4b1aed6
JeliHacker <jeligooch@gmail.com>
2022-04-18 13:31:43 -0400

    successfully linked the css sheet, currently works for all pages

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.io.File;
+import java.net.MalformedURLException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,6 +14,17 @@ import views.LoginController;
 public class Main extends Application
 {
 
+	//This function is so we can use our css stylesheet, it convert the url to a string
+	//https://stackoverflow.com/questions/33434588/eclipse-javafx-css-resource-not-found
+	public static String fileToStylesheetString (File stylesheetFile) {
+	    try {
+	        return stylesheetFile.toURI().toURL().toString();
+	    } catch ( MalformedURLException e ) {
+	        return null;
+	    }
+	}
+	
+	
 	@Override
 	public void start(Stage stage) throws Exception
 	{
@@ -21,7 +35,24 @@ public class Main extends Application
 		navigation_model vm = new navigation_model(view);
 		cont.setModel(vm);
 		
+		
+		
+	
+		
+		
 		Scene s = new Scene(view);
+		
+		
+		// using the function we created earlier to convert the stylesheet url to a string, then adding it
+		String fontSheet = fileToStylesheetString(new File ("src/main/java/views/style.css"));
+
+		if (fontSheet == null) {
+		    //Do Whatever you want with logging/errors/etc.
+			System.out.println("CSS sheet not found!");
+		} else {
+		    s.getStylesheets().add( fontSheet );
+		}
+
 		stage.setScene(s);
 		stage.show();	
 
@@ -30,6 +61,8 @@ public class Main extends Application
 	
 	public static void main(String[] args)
 	{
+		
+		
 		launch(args);
 	}
 }
